09328191d42b218728fb6b5259b19f99f59d0309
yashika-innodeed <yashika.lalwani@innodeed.com>
2023-09-06 17:32:55 +0530

    button addition

--- antdprojectn/src/index.js
+++ antdprojectn/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ButtonJS from './antDButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ButtonJS />
   </React.StrictMode>
 );
 
